a64f45b000c1ab514d5baaf92ef2cc1fdfa68519
Your Name <you@example.com>
2024-03-13 11:31:56 +0900

    파이썬 반복문

--- hello.py
+++ hello.py
@@ -2,4 +2,15 @@ print("hello")
 print("chaehyeon")
 print("이것은 내가 처음 만든 파이썬 프로그램 입니다")
 print("Hi")
-print("헬로우")
+print(5+5)
+
+
+
+sum = 0
+n = 1
+
+while n <= 10 :
+    sum = sum + n
+    n = n + 1
+
+print(sum)
